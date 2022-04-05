73babe5bf6c05a88a9c9a992d27ca5c43f7bf488
Александр Акантьев <akant@MacBook-Pro.local>
2022-04-05 15:45:58 +0300

    4th try of resolving conflict

--- MyBank/src/main/java/com/Vassah/MyBank/Controllers/homecontroller.java
+++ MyBank/src/main/java/com/Vassah/MyBank/Controllers/homecontroller.java
@@ -2,6 +2,7 @@ package com.Vassah.MyBank.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
