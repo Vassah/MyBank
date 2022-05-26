88a7a96f82ae892cd459a016c9f6aadb9c2694ca
Александр Акантьев <akantyevad@gmail.com>
2022-05-26 15:49:29 +0300

    added methods to controllers

--- MyBank/src/main/java/com/Vassah/MyBank/Controllers/RegistrationController.java
+++ MyBank/src/main/java/com/Vassah/MyBank/Controllers/RegistrationController.java
@@ -1,20 +1,18 @@
 package com.Vassah.MyBank.Controllers;
 
 import com.Vassah.MyBank.Repositories.UserRepository;
+import com.Vassah.MyBank.Services.UserBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-/*import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-*/
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class RegistrationController {
@@ -22,8 +20,30 @@ public class RegistrationController {
     @Autowired
     private UserRepository userRepo;
 
+    public RegistrationController(UserRepository _userRepoo)
+    {
+        userRepo = _userRepoo;
+    }
+
+    @GetMapping("/registration")
+    public String RegisterUser()
+    {
+        return "registration_page";
+    }
+
+    @PostMapping(value="/registration")
+    public String RegisterUser(@RequestBody UserBuilder Name) {
+        //TODO: process POST request
+        
+        return "registration/phone";
+    }
+
+    @GetMapping("registration/phone")
+    public String PhoneAdmit()
+    {
+        return "registration/phone";
+    }
     
-    public RegistrationController(){}
     
 
 }
