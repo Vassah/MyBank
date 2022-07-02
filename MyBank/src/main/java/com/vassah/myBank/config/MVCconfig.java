5dde5908ca9ea52920e3830f5737adf2f12d3af9
vaas <VasyaDova@yandex.ru>
2022-07-02 22:41:01 +0300

    Our-services and contact-us pages in folder we

--- MyBank/src/main/java/com/vassah/myBank/config/MVCconfig.java
+++ MyBank/src/main/java/com/vassah/myBank/config/MVCconfig.java
@@ -12,11 +12,15 @@ public class MVCconfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/logout").setViewName("logout");
         registry.addViewController("/" ).setViewName("index");
-        registry.addViewController("/about-us").setViewName("about-us");
+        registry.addViewController("/about-us").setViewName("we/about-us");
         registry.addViewController("/404").setViewName("error/404");
         registry.addViewController("/403").setViewName("error/403");
         registry.addViewController("/ConfirmEmail").setViewName("ConfirmEmail");
         registry.addViewController("/ConfirmEmail/success").setViewName("EmailConfirmed");
         registry.addViewController("/ConfirmEmail/fail").setViewName("EmailConfirmFailed");
+        registry.addViewController("/contact-us").setViewName("we/contact-us");
+        registry.addViewController("/our-services").setViewName("we/our-services");
+
+
     }
 }
